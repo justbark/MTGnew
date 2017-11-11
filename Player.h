d1944c60909f7be1bde940812fc024d5a5a32cfd
justbark <justbark89@gmail.com>
2017-11-10 23:40:15 -0600

    added player header

--- Player.h
+++ Player.h
@@ -1,1 +1,42 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Deck.h"
+#include <iostream>
+#include <string>
+
+class Player 
+{
+public:
+	//constructor
+	Player();
+
+	//functions
+	std::string getPlayerName();
+	int getPlayerLife();
+	int getPlayerPoisoncounters();
+
+
+private:
+	std::string playerName;
+	int playerLife;
+	int playerPoisonCounters;
+	Deck playerDeck;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // !PLAYER_H
+
